5960f54a4d3fc568e66e61aaa215bcb420996711
wlghdu97 <jiho@xharpen.com>
2020-01-28 22:40:56 +0900

    Add dark theme property to GeneralPreference

--- shared/src/main/java/com/xhlab/nep/shared/preference/GeneralPreference.kt
+++ shared/src/main/java/com/xhlab/nep/shared/preference/GeneralPreference.kt
@@ -6,4 +6,8 @@ interface GeneralPreference {
     val isDBLoaded: LiveData<Boolean>
     fun getDBLoaded(): Boolean
     fun setDBLoaded(value: Boolean)
+
+    val isDarkTheme: LiveData<Boolean?>
+    fun getDarkTheme(): Boolean
+    fun setDarkTheme(value: Boolean)
 }
--- shared/src/main/java/com/xhlab/nep/shared/preference/GeneralSharedPreference.kt
+++ shared/src/main/java/com/xhlab/nep/shared/preference/GeneralSharedPreference.kt
@@ -1,40 +1,54 @@
 package com.xhlab.nep.shared.preference
 
 import android.app.Application
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.preference.PreferenceManager
+import com.xhlab.nep.shared.R
 import javax.inject.Inject
 
-class GeneralSharedPreference @Inject constructor(app: Application) : GeneralPreference {
+class GeneralSharedPreference @Inject constructor(
+    private val app: Application
+) : GeneralPreference, SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private val preferenceChangeListener = { _: SharedPreferences?, key: String? ->
-        if (key == DB_LOADED) {
-            _isDBLoaded.value = getDBLoaded()
+    private val preference = PreferenceManager
+        .getDefaultSharedPreferences(app.applicationContext).apply {
+            registerOnSharedPreferenceChangeListener(this@GeneralSharedPreference)
         }
-    }
-
-    private val preference: SharedPreferences by lazy {
-        app.applicationContext.getSharedPreferences(GENERAL_PREFERENCE, MODE_PRIVATE).apply {
-            registerOnSharedPreferenceChangeListener(preferenceChangeListener)
-        }
-    }
 
     private val _isDBLoaded = MutableLiveData<Boolean>(getDBLoaded())
     override val isDBLoaded: LiveData<Boolean>
         get() = _isDBLoaded
 
+    private val _isDarkTheme = MutableLiveData<Boolean?>(getDarkTheme())
+    override val isDarkTheme: LiveData<Boolean?>
+        get() = _isDarkTheme
+
     override fun getDBLoaded(): Boolean {
-        return preference.getBoolean(DB_LOADED, false)
+        return preference.getBoolean(app.getString(R.string.key_db_status), false)
+    }
+
+    override fun getDarkTheme(): Boolean {
+        return preference.getBoolean(app.getString(R.string.key_theme), false)
     }
 
     override fun setDBLoaded(value: Boolean) {
-        preference.edit().putBoolean(DB_LOADED, value).apply()
+        preference.edit().putBoolean(app.getString(R.string.key_db_status), value).apply()
+        _isDBLoaded.postValue(value)
     }
 
-    companion object {
-        private const val GENERAL_PREFERENCE = "general_preference"
-        private const val DB_LOADED = "isDBLoaded"
+    override fun setDarkTheme(value: Boolean) {
+        preference.edit().putBoolean(app.getString(R.string.key_theme), value).apply()
+        _isDarkTheme.postValue(value)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when (key) {
+            app.getString(R.string.key_db_status) ->
+                _isDBLoaded.value = getDBLoaded()
+            app.getString(R.string.key_theme) ->
+                _isDarkTheme.value = getDarkTheme()
+        }
     }
 }
