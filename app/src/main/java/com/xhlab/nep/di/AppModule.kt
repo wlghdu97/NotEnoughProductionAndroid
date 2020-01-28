bd68ccb118e4b512cfc6a37ada86f18bf7a2782d
wlghdu97 <jiho@xharpen.com>
2020-01-28 22:43:03 +0900

    Add ThemeDelegate

--- app/src/main/java/com/xhlab/nep/di/AppModule.kt
+++ app/src/main/java/com/xhlab/nep/di/AppModule.kt
@@ -1,9 +1,18 @@
 package com.xhlab.nep.di
 
 import com.xhlab.nep.shared.di.SharedModule
+import com.xhlab.nep.shared.preference.GeneralPreference
+import com.xhlab.nep.ui.delegate.ThemeDelegate
+import com.xhlab.nep.ui.delegate.ThemeDelegateImpl
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module(includes = [SharedModule::class])
 class AppModule {
-
+    @Provides
+    @Singleton
+    fun provideThemeDelegate(
+        generalPreference: GeneralPreference
+    ): ThemeDelegate = ThemeDelegateImpl(generalPreference)
 }
