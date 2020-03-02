f1b468487c3c1490479738e93ec1b2eb72cffbe4
wlghdu97 <jiho@xharpen.com>
2020-03-02 16:49:34 +0900

    Add view pager adapter to ElementDetailActivity

--- app/src/main/java/com/xhlab/nep/ui/element/ElementDetailActivity.kt
+++ app/src/main/java/com/xhlab/nep/ui/element/ElementDetailActivity.kt
@@ -3,10 +3,18 @@ package com.xhlab.nep.ui.element
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.xhlab.nep.R
 import com.xhlab.nep.di.ViewModelFactory
 import com.xhlab.nep.shared.db.entity.ElementEntity.Companion.ORE_CHAIN
 import com.xhlab.nep.ui.ViewInit
+import com.xhlab.nep.ui.element.recipes.RecipeListFragment
+import com.xhlab.nep.ui.element.replacements.ReplacementContainerFragment
+import com.xhlab.nep.ui.element.usages.UsageListFragment
 import com.xhlab.nep.util.observeNotNull
 import com.xhlab.nep.util.viewModelProvider
 import dagger.android.support.DaggerAppCompatActivity
@@ -19,6 +27,8 @@ class ElementDetailActivity : DaggerAppCompatActivity(), ViewInit {
     lateinit var viewModelFactory: ViewModelFactory
 
     private lateinit var viewModel: ElementDetailViewModel
+
+    private lateinit var viewPagerAdapter: FragmentStatePagerAdapter
 
     private var elementId: Long = 0L
     private var elementType: Int = -1
@@ -60,6 +70,64 @@ class ElementDetailActivity : DaggerAppCompatActivity(), ViewInit {
                 else -> R.string.tab_recipes
             })
         }
+
+        with (view_pager) {
+            viewPagerAdapter = when (elementType) {
+                ORE_CHAIN -> ReplacementListPagerAdapter(supportFragmentManager)
+                else -> RecipeListPagerAdapter(supportFragmentManager)
+            }
+            adapter = viewPagerAdapter
+            addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+                override fun onPageSelected(position: Int) {
+                    tab_layout.selectTab(tab_layout.getTabAt(position))
+                }
+            })
+        }
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                view_pager.currentItem = tab.position
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) = Unit
+            override fun onTabUnselected(tab: TabLayout.Tab) = Unit
+        })
+    }
+
+    private inner class RecipeListPagerAdapter(
+        fragmentManager: FragmentManager
+    ) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        private val recipeListFragment by lazy { RecipeListFragment() }
+        private val usageListFragment by lazy { UsageListFragment() }
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> recipeListFragment
+                1 -> usageListFragment
+                else -> Fragment()
+            }.apply { arguments = Bundle().apply { putLong(ELEMENT_ID, elementId) } }
+        }
+
+        override fun getCount() = 2
+    }
+
+    private inner class ReplacementListPagerAdapter(
+        fragmentManager: FragmentManager
+    ) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        private val replacementContainerFragment by lazy { ReplacementContainerFragment() }
+        private val usageListFragment by lazy { UsageListFragment() }
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> replacementContainerFragment
+                1 -> usageListFragment
+                else -> Fragment()
+            }.apply { arguments = Bundle().apply { putLong(ELEMENT_ID, elementId) } }
+        }
+
+        override fun getCount() = 2
     }
 
     companion object {
--- app/src/main/java/com/xhlab/nep/ui/element/ElementDetailModule.kt
+++ app/src/main/java/com/xhlab/nep/ui/element/ElementDetailModule.kt
@@ -4,12 +4,17 @@ import androidx.lifecycle.ViewModel
 import com.xhlab.nep.di.ViewModelKey
 import com.xhlab.nep.di.scopes.FragmentScope
 import com.xhlab.nep.ui.element.recipes.RecipeListFragment
+import com.xhlab.nep.ui.element.recipes.RecipeListModule
+import com.xhlab.nep.ui.element.replacements.ReplacementContainerFragment
+import com.xhlab.nep.ui.element.replacements.ReplacementContainerModule
+import com.xhlab.nep.ui.element.usages.UsageListFragment
+import com.xhlab.nep.ui.element.usages.UsageListModule
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import dagger.multibindings.IntoMap
 
-@Module
+@Module(includes = [RecipeListModule::class, ReplacementContainerModule::class, UsageListModule::class])
 @Suppress("unused")
 abstract class ElementDetailModule {
     @Binds
@@ -20,4 +25,12 @@ abstract class ElementDetailModule {
     @FragmentScope
     @ContributesAndroidInjector
     abstract fun provideRecipeListFragment(): RecipeListFragment
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    abstract fun provideReplacementContainerFragment(): ReplacementContainerFragment
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    abstract fun provideUsageListFragment(): UsageListFragment
 }
