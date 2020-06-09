c303862ecc8795778b6dff0737b0c61491b9e1d6
wlghdu97 <jiho@xharpen.com>
2020-06-09 13:16:00 +0900

    Change UseCase to run without active LiveData

--- shared/src/main/java/com/xhlab/nep/shared/domain/UseCase.kt
+++ shared/src/main/java/com/xhlab/nep/shared/domain/UseCase.kt
@@ -2,9 +2,10 @@ package com.xhlab.nep.shared.domain
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.liveData
 import com.xhlab.nep.shared.util.Resource
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 abstract class UseCase<in Params, Result> {
@@ -12,23 +13,28 @@ abstract class UseCase<in Params, Result> {
     protected abstract suspend fun execute(params: Params): Result
 
     operator fun invoke(params: Params, resultData: MutableLiveData<Resource<Result>>) {
-        resultData.value = Resource.loading(null)
-
-        val result = invokeFunction(params)
-        result.observeForever { resultData.value = it }
+        CoroutineScope(SupervisorJob()).launch {
+            try {
+                resultData.postValue(Resource.success(execute(params)))
+            } catch (e: Exception) {
+                Timber.e(e)
+                resultData.postValue(Resource.error(e))
+            }
+        }
     }
 
     operator fun invoke(params: Params): LiveData<Resource<Result>> {
-        return invokeFunction(params)
+        val result = MutableLiveData<Resource<Result>>()
+        invoke(params, result)
+        return result
     }
 
-    private fun invokeFunction(params: Params) = liveData(SupervisorJob()) {
-        try {
-            val result = execute(params)
-            emit(Resource.success(result))
+    suspend fun invokeInstant(params: Params): Resource<Result> {
+        return try {
+            Resource.success(execute(params))
         } catch (e: Exception) {
             Timber.e(e)
-            emit(Resource.error(e))
+            Resource.error(e)
         }
     }
 }
