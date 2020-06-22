8b598f460eb7a46367c93d0e0f5ed309ef2738c6
wlghdu97 <jiho@xharpen.com>
2020-06-22 21:34:19 +0900

    Fix each branch to have its own SharedModule

--- shared/src/staging/java/com/xhlab/nep/shared/di/SharedModule.kt
+++ shared/src/staging/java/com/xhlab/nep/shared/di/SharedModule.kt
@@ -3,10 +3,10 @@ package com.xhlab.nep.shared.di
 import android.app.Application
 import com.xhlab.nep.shared.data.element.ElementRepo
 import com.xhlab.nep.shared.data.element.ElementRepoImpl
-import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepo
-import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepoImpl
 import com.xhlab.nep.shared.data.machine.MachineRepo
 import com.xhlab.nep.shared.data.machine.MachineRepoImpl
+import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepo
+import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepoImpl
 import com.xhlab.nep.shared.data.oredict.OreDictRepo
 import com.xhlab.nep.shared.data.oredict.OreDictRepoImpl
 import com.xhlab.nep.shared.data.oredict.ReplacementAdder
@@ -14,14 +14,15 @@ import com.xhlab.nep.shared.data.recipe.RecipeAdder
 import com.xhlab.nep.shared.data.recipe.RecipeRepo
 import com.xhlab.nep.shared.data.recipe.RecipeRepoImpl
 import com.xhlab.nep.shared.db.AppDatabase
+import com.xhlab.nep.shared.di.RoomModule
 import com.xhlab.nep.shared.preference.GeneralPreference
 import com.xhlab.nep.shared.preference.GeneralSharedPreference
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Suppress("unused")
 @Module(includes = [RoomModule::class])
+@Suppress("unused")
 class SharedModule {
     @Provides
     @Singleton
