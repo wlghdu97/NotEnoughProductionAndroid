6e75bc4dc280d7d12789e209aa884553882e8a2c
wlghdu97 <jiho@xharpen.com>
2020-06-25 15:22:56 +0900

    Add temporary process repo implementation

--- shared/src/debugRelease/java/com/xhlab/nep/shared/di/SharedModule.kt
+++ shared/src/debugRelease/java/com/xhlab/nep/shared/di/SharedModule.kt
@@ -1,20 +1,21 @@
-package com.xhlab.nep.di
+package com.xhlab.nep.shared.di
 
 import android.app.Application
 import com.xhlab.nep.shared.data.element.ElementRepo
 import com.xhlab.nep.shared.data.element.ElementRepoImpl
-import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepo
-import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepoImpl
 import com.xhlab.nep.shared.data.machine.MachineRepo
 import com.xhlab.nep.shared.data.machine.MachineRepoImpl
+import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepo
+import com.xhlab.nep.shared.data.machinerecipe.MachineRecipeRepoImpl
 import com.xhlab.nep.shared.data.oredict.OreDictRepo
 import com.xhlab.nep.shared.data.oredict.OreDictRepoImpl
 import com.xhlab.nep.shared.data.oredict.ReplacementAdder
+import com.xhlab.nep.shared.data.process.ProcessRepo
+import com.xhlab.nep.shared.data.process.ProcessRepoImpl
 import com.xhlab.nep.shared.data.recipe.RecipeAdder
 import com.xhlab.nep.shared.data.recipe.RecipeRepo
 import com.xhlab.nep.shared.data.recipe.RecipeRepoImpl
 import com.xhlab.nep.shared.db.AppDatabase
-import com.xhlab.nep.shared.di.RoomModule
 import com.xhlab.nep.shared.preference.GeneralPreference
 import com.xhlab.nep.shared.preference.GeneralSharedPreference
 import dagger.Module
@@ -47,4 +48,8 @@ class SharedModule {
     @Provides
     @Singleton
     internal fun provideMachineRecipeRepo(db: AppDatabase): MachineRecipeRepo = MachineRecipeRepoImpl(db)
+
+    @Provides
+    @Singleton
+    internal fun provideProcessRepo(): ProcessRepo = ProcessRepoImpl()
 }
