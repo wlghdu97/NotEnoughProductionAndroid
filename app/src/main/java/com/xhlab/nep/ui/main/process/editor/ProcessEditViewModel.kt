6f26da2e125e93dc9732d9edf1560c15f0258802
wlghdu97 <jiho@xharpen.com>
2020-06-25 15:16:38 +0900

    Add process tree adapter

---
+++ app/src/main/java/com/xhlab/nep/ui/main/process/editor/ProcessEditViewModel.kt
@@ -0,0 +1,39 @@
+package com.xhlab.nep.ui.main.process.editor
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.xhlab.nep.shared.domain.process.LoadProcessUseCase
+import com.xhlab.nep.shared.preference.GeneralPreference
+import com.xhlab.nep.shared.util.Resource
+import com.xhlab.nep.ui.BaseViewModel
+import com.xhlab.nep.ui.BasicViewModel
+import javax.inject.Inject
+
+class ProcessEditViewModel @Inject constructor(
+    private val loadProcessUseCase: LoadProcessUseCase,
+    generalPreference: GeneralPreference
+) : ViewModel(), BaseViewModel by BasicViewModel() {
+
+    val process = loadProcessUseCase.observeOnly(Resource.Status.SUCCESS)
+
+    val isIconLoaded = generalPreference.isIconLoaded
+
+    private val _iconMode = MutableLiveData<Boolean>(true)
+    val iconMode: LiveData<Boolean>
+        get() = _iconMode
+
+    fun init(processId: String?) {
+        if (processId == null) {
+            throw NullPointerException("process id is null.")
+        }
+        invokeMediatorUseCase(
+            useCase = loadProcessUseCase,
+            params = LoadProcessUseCase.Parameter(processId)
+        )
+    }
+
+    fun toggleIconMode() {
+        _iconMode.postValue(_iconMode.value != true)
+    }
+}
