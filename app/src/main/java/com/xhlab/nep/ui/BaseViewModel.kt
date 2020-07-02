f5e37c8f443660a3af9bc16845c86ae575b01e12
wlghdu97 <jiho@xharpen.com>
2020-07-02 19:02:16 +0900

    Add BaseViewModel.launchSuspendFunction

--- app/src/main/java/com/xhlab/nep/ui/BaseViewModel.kt
+++ app/src/main/java/com/xhlab/nep/ui/BaseViewModel.kt
@@ -2,9 +2,13 @@ package com.xhlab.nep.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.xhlab.nep.shared.domain.MediatorUseCase
 import com.xhlab.nep.shared.domain.UseCase
 import com.xhlab.nep.shared.util.Resource
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class BasicViewModel : BaseViewModel {
     override val refreshStatus = MediatorLiveData<Resource.Status>()
@@ -13,6 +17,19 @@ class BasicViewModel : BaseViewModel {
 interface BaseViewModel {
 
     val refreshStatus: MediatorLiveData<Resource.Status>
+
+    fun <Result> ViewModel.launchSuspendFunction(
+        resultData: MediatorLiveData<Resource<Result>>? = null,
+        function: suspend () -> Result
+    ) {
+        val handler = CoroutineExceptionHandler { _, t ->
+            resultData?.postValue(Resource.error(t))
+        }
+        viewModelScope.launch(handler) {
+            val result = function()
+            resultData?.postValue(Resource.success(result))
+        }
+    }
 
     fun <Params, Result> invokeMediatorUseCase(
         useCase: MediatorUseCase<Params, Result>,
