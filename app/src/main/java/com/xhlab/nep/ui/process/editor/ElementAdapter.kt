a8119a3fb5edfbfda7bd6c40b7635a5aca6707e9
wlghdu97 <jiho@xharpen.com>
2020-07-05 18:59:01 +0900

    Add connecting to parent degree limitation

--- app/src/main/java/com/xhlab/nep/ui/process/editor/ElementAdapter.kt
+++ app/src/main/java/com/xhlab/nep/ui/process/editor/ElementAdapter.kt
@@ -56,9 +56,10 @@ class ElementAdapter(
             }
 
             connectionStatus.status.let {
+                val degree = recipeNode?.degree ?: 0
                 menuButton.isGone = (it == FINAL_OUTPUT)
                 disconnect.isVisible = (it == CONNECTED_TO_PARENT || it == CONNECTED_TO_CHILD)
-                connectToParent.isVisible = (it != FINAL_OUTPUT || it != NOT_CONSUMED)
+                connectToParent.isVisible = (degree != 0 && (it != FINAL_OUTPUT || it != NOT_CONSUMED))
                 connectToChild.isVisible = (it != FINAL_OUTPUT || it != NOT_CONSUMED)
                 markNotConsumed.isVisible = (it == UNCONNECTED || it == NOT_CONSUMED)
                 markNotConsumed.setTitle(when (it == UNCONNECTED) {
