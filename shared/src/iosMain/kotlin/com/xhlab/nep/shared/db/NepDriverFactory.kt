085af19cacdd880d29516316add60ba97ca322a7
wlghdu97 <inganlegi@gmail.com>
2021-11-22 18:26:32 +0900

    Enable foreign key constraint for sqldelight native driver

--- shared/src/iosMain/kotlin/com/xhlab/nep/shared/db/NepDriverFactory.kt
+++ shared/src/iosMain/kotlin/com/xhlab/nep/shared/db/NepDriverFactory.kt
@@ -1,11 +1,27 @@
 package com.xhlab.nep.shared.db
 
+import co.touchlab.sqliter.DatabaseConfiguration
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+import com.squareup.sqldelight.drivers.native.wrapConnection
 
 actual class NepDriverFactory {
 
     actual fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(Nep.Schema, NEP_DB_NAME)
+        val schema = Nep.Schema
+        val config = DatabaseConfiguration(
+            name = NEP_DB_NAME,
+            version = schema.version,
+            create = { connection ->
+                wrapConnection(connection) { schema.create(it) }
+            },
+            upgrade = { connection, oldVersion, newVersion ->
+                wrapConnection(connection) { schema.migrate(it, oldVersion, newVersion) }
+            },
+            extendedConfig = DatabaseConfiguration.Extended(
+                foreignKeyConstraints = true
+            )
+        )
+        return NativeSqliteDriver(config)
     }
 }
--- shared/src/iosMain/kotlin/com/xhlab/nep/shared/db/ProcessDriverFactory.kt
+++ shared/src/iosMain/kotlin/com/xhlab/nep/shared/db/ProcessDriverFactory.kt
@@ -1,11 +1,27 @@
 package com.xhlab.nep.shared.db
 
+import co.touchlab.sqliter.DatabaseConfiguration
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+import com.squareup.sqldelight.drivers.native.wrapConnection
 
 actual class ProcessDriverFactory {
 
     actual fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(NepProcess.Schema, PROCESS_DB_NAME)
+        val schema = NepProcess.Schema
+        val config = DatabaseConfiguration(
+            name = PROCESS_DB_NAME,
+            version = schema.version,
+            create = { connection ->
+                wrapConnection(connection) { schema.create(it) }
+            },
+            upgrade = { connection, oldVersion, newVersion ->
+                wrapConnection(connection) { schema.migrate(it, oldVersion, newVersion) }
+            },
+            extendedConfig = DatabaseConfiguration.Extended(
+                foreignKeyConstraints = true
+            )
+        )
+        return NativeSqliteDriver(config)
     }
 }
